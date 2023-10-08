2db1eddd82172909890ee220a04dc91c5d3b1337
TB <114008066+Tbusk@users.noreply.github.com>
2023-10-08 01:51:37 -0400

    Adding SQL for Database, Table, and Products

---
+++ mariadb.sql
@@ -0,0 +1,49 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.2.1-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for store
+CREATE DATABASE IF NOT EXISTS `store` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `store`;
+
+-- Dumping structure for table store.product
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `short_title` varchar(100) NOT NULL,
+  `long_title` varchar(250) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `image_url` varchar(250) DEFAULT NULL,
+  `rating` float unsigned NOT NULL DEFAULT 0,
+  `price` decimal(20,2) unsigned NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table store.product: ~7 rows (approximately)
+INSERT INTO `product` (`id`, `short_title`, `long_title`, `description`, `category`, `image_url`, `rating`, `price`) VALUES
+	(10, 'Fjallraven Foldsack Backpack', 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops', 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday', 'men\'s clothing', 'http://localhost:8080/ecommerce/assets/images/Fjallraven%20-%20Foldsack%20Backpack.png', 3.9, 109.95),
+	(11, 'Mens Slim Fit T-Shirt', 'Mens Casual Premium Slim Fit T-Shirts', 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.', 'men\'s clothing', 'http://localhost:8080/ecommerce/assets/images/Mens%20Slim%20Fit%20T-Shirt.jpg', 4.1, 22.30),
+	(12, 'Mens Cotton Jacket', 'Mens Cotton Jacket', 'Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.', 'men\'s clothing', 'http://localhost:8080/ecommerce/assets/images/Mens%20Cotton%20Jacket.jpg', 4.7, 55.99),
+	(13, 'Western Digital 2TB External HDD', 'WD 2TB Elements Portable External Hard Drive - USB 3.0', 'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system', 'electronics', 'http://localhost:8080/ecommerce/assets/images/WD%202TB%20External%20HDD.png', 3.3, 64.00),
+	(14, 'SanDisk SSD PLUS 1TB Internal SSD', 'SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s', 'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.', 'electronics', 'http://localhost:8080/ecommerce/assets/images/sandiskssd.png', 2.9, 109.00),
+	(15, 'Silicon Power 256GB SSD', 'Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5', '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.', 'electronics', 'http://localhost:8080/ecommerce/assets/images/siliconpowerssd.png', 4.8, 112.00),
+	(16, 'Samsung 49-Inch Curved Gaming Monitor', 'Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED', '49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag', 'electronics', 'http://localhost:8080/ecommerce/assets/images/samsung49inchmonitor.png', 2.2, 999.99);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
